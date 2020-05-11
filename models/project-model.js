b4b637641aae68dafb88afefa367920197fb1e17
ASANCHEZWEB <desarrollosalbertosanchez@gmail.com>
2020-05-11 12:31:14 +0200

    añadir id creador al modelo proyecto , actualizar ruta

--- models/project-model.js
+++ models/project-model.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
+const User = require('./user-model');
 
 const projectSchema = new Schema({
   title: String,
   description: String,
   tasks: [{type: Schema.Types.ObjectId, ref: 'Task'}],
-  // owner will be added later on
+  owner: {type: Schema.Types.ObjectId, ref: 'User'}
 });
 
 const Project = mongoose.model('Project', projectSchema);
